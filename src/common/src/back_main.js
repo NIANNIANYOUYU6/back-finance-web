e21b63dbc9153657123ae0a4619e2f008edec12d
kuilaurence <kuilaurence@163.com>
2021-03-25 21:50:25 +0800

    update   sdk

--- src/common/src/back_main.js
+++ src/common/src/back_main.js
@@ -563,7 +563,7 @@ export async function getPairList() {
             interestAPY0: pair.interestAPY0,
             interestAPY1: pair.interestAPY1,
             swapperName: swapper[pair.pid],
-            liquidityAPY: getLPAPR([getTokenSymbol(pair.token0) + "/" + etTokenSymbol(pair.token1)]),
+            liquidityAPY: getLPAPR([getTokenSymbol(pair.token0) + "/" + getTokenSymbol(pair.token1)]),
         };
         list.push(item);
     }

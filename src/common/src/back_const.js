d5636011b09ec10c0b930733501b5ca53902c060
kuilaurence <kuilaurence@163.com>
2021-03-29 18:06:38 +0800

    remove some address

--- src/common/src/back_const.js
+++ src/common/src/back_const.js
@@ -16,30 +16,19 @@ export const bourse = {
 export const MAXNUM = 9999 * 99999 * 100000;
 export const ContractAddress = {
     97: {
-        mdexFactoryContract: "0xa9E74501a8823Cd31A87E7848ABE91f849aeF2B6",
-        mdexRouterContract: "0xa119aEfb3426c4A77ed5c0113789deA1B697c222",
-        mdexPoolContract: "0xc97F230a3f18280B4c4Ab1992dC45E4A51a93888",
         mdexToken: '0xA7ADac24706c3b8414A6b81bCd179b54e01653f0',
-        WETH: "0x8C6E78e8598511f83280E4c1B18b40de8615224f",
-
         backToken: "0xc0Ff7641b73790d2521b4f3baE605A16F8C6787B",
-        backPlatformContract: "0x5917878B3d5bA8A330755AD5Fb3E22851553ae7F",
         mdexSwapperContract: '0x956136863f81E930357e5599c5C4A3F78278505b',
         backFactoryContract: '0x834edB598bDac7F1a65Cf6DC0c759Ef2ad81EEB3',
 
-        backPoolFactoryContract: "0x03b2149356e93eCa3f1273216B190b82d4D44330",
-        backPairFactoryContract: "0x83ca11503620b8823bd1289D7A09D4f34143EceA",
-        backConfigContract: '0xAbA043Ff0697df5bCb0E8A7cbfe248a9Ab926109',
-        backQueryContract: "0x83B237EfA39bd0DA6064562e4ADC0A2F986CfC50",
         usdtmdex: '0x30686895109247C0376bcaE60Fcb6Cbc03165f33',
+        backQueryContract: "0x83B237EfA39bd0DA6064562e4ADC0A2F986CfC50",
 
-        BXHPoolContract: "0x74c7b7F41cFC55a52347800aDC1A46e2D03Fdb24",
-        BXHFactoryContract: "0xe18B48a42cCE7E731FD5694c9eD9d35720FcBBA9",
-        BXHRouterContract: "0xbE1d4d1824Af7cC9D575065800725fdecE2176Fe",
-        BXHSwapperContract: "0x213146FE40511Dcda9451E7b734F82c892085D47",
-        bxhlpUSDTBTC: "0x8597bde001778030623646a91169201163fA49C1",
-        bxhlpUSDTETH: "0x794F16D9AbeE365d3Cf469F230c9b1AFDd9FD38d",
-        bxhlpETHBTC: "0xa394eFFaaA75120358bf955Ac08CDe7BeA2A02cD",
+        backConfigContract: '0xAbA043Ff0697df5bCb0E8A7cbfe248a9Ab926109',
+        backPairFactoryContract: "0x83ca11503620b8823bd1289D7A09D4f34143EceA",
+        backPlatformContract: "0x5917878B3d5bA8A330755AD5Fb3E22851553ae7F",
+        mdexFactoryContract: "0xa9E74501a8823Cd31A87E7848ABE91f849aeF2B6",
+        backPoolFactoryContract: "0x03b2149356e93eCa3f1273216B190b82d4D44330",
     },
     128: {
         mdexToken: '0x012b9af79C4DdD265D581108A5642FC46F32076f',

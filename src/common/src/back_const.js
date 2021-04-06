aab0ece0e3cd077b40d41c3fd20b078e47a79145
Jie Yang <jie.yang@nanchao.org>
2021-04-06 14:34:16 +0800

    Update for contract

--- src/common/src/back_const.js
+++ src/common/src/back_const.js
@@ -18,18 +18,18 @@ export const backNode = "http://18.181.217.60:8000/subgraphs/name/example";
 export const MAXNUM = 9999 * 99999 * 100000;
 export const ContractAddress = {
     97: {
-        mdexToken: '0xaaE0d85526cA55D933704c7707B6a1A233Df5Fe8',
-        backToken: '0x28A0A2aE5b2f3459dDE1C541FcC90e8b3033438c',
-        mdexSwapperContract: '0xe889eA0b0Dc1f60902c4DCC4b1f9B16eF2728450',
+        mdexToken: '0x59cEcc274ebF18A0B3c51393800360807D868419',
+        backToken: '0xdC658b2A40A7c9F6E20Cce520cA73694c91BAAfB',
+        mdexSwapperContract: '0xe022607DDAAe2a73a28174a00D12434e6c7F4Ed0',
 
         usdtmdex: '0x30686895109247C0376bcaE60Fcb6Cbc03165f33',
-        backQueryContract: "0x3d04B6AcAC25feC4684324A792441732280B0458",
+        backQueryContract: "0xd7410B314A659407786a09E2E3eAAf5c3D2ad72D",
 
-        backConfigContract: '0xe86cba30B44F3818934c580aDCC52E365DB6767B',
-        backPairFactoryContract: "0xD7498a8dE66D41412373B84b12d0AA65147E2aCc",
-        backPlatformContract: "0x8aE30A4fd6eF1bdb27f9E5B727F2D3816c363DcC",
-        backPoolFactoryContract: "0x6ca2cD674Ec312479ecD71e6b4142eb4a7599E08",
-        mdexFactoryContract: "0x9B1bDEA897c643649D0D826b777Fd8F123AD70af",
+        backConfigContract: '0xa94848Aab553787D507f26641E1b854076494732',
+        backPairFactoryContract: "0x3546EcfE44F4A069Cb9Fd031300dbc8d0629A1fe",
+        backPlatformContract: "0x77d1ee62e9A98374C6Cd955b54a7CAF90A3A2134",
+        backPoolFactoryContract: "0x720d68Acf67B43aF20cd3db27Ad6d70dC72193c7",
+        mdexFactoryContract: "0x0F3ab99ec2a2E72231F6e84828b7389D2A6663CC",
     },
     128: {
         mdexToken: '0x012b9af79C4DdD265D581108A5642FC46F32076f',
@@ -45,12 +45,12 @@ export const ContractAddress = {
 
 export const tokenAddres = {
     97: {
-        USDT: '0xf79e65Ad5b5E4EC6e0b6C1D846a8c6B832318402',
-        BTC: '0xd2326A915E94578E3c8b7d859DfEa2b9C1aCb1e1',
-        ETH: '0xA9F648E832AB544289055335bb120E75893c06C8',
-        BACK: "0x28A0A2aE5b2f3459dDE1C541FcC90e8b3033438c",
-        MDEX: "0xaaE0d85526cA55D933704c7707B6a1A233Df5Fe8",
-        BXH: "0xA38714ef007e17Bfbb631c820588D490Dd5191E1",
+        USDT: '0x3F145283EEA7118B56b6C5fa794b68D0C0832318',
+        BTC: '0x9F4231f4649fF552ca445709e9C69c90B3AF4Bd4',
+        ETH: '0xFe9BE12B304ffbDCcC6a7D2189Bf0C6b2CDf2812',
+        BACK: "0xdC658b2A40A7c9F6E20Cce520cA73694c91BAAfB",
+        MDEX: "0x59cEcc274ebF18A0B3c51393800360807D868419",
+        BXH: "0x8775B61F6FBD08971f42C958C460BE2Ef6269962",
     },
     128: {
         USDT: '0x721aEda9c81ADAc92dFc9248127C219D91036671',
